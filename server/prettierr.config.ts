a62621788eb823af8675581a912e52e91e0e2cfa
unknown <seighitm@mail.ru>
2024-09-03 08:04:59 +0300

    add server configs

---
+++ server/prettierr.config.ts
@@ -0,0 +1,13 @@
+import {Options} from 'prettier';
+
+const config: Options = {
+    "printWidth": 80,
+    "tabWidth": 2,
+    "useTabs": false,
+    "semi": true,
+    "singleQuote": true,
+    "trailingComma": "all",
+    "bracketSpacing": true,
+};
+
+export default config;
